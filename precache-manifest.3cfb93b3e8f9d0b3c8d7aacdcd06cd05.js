496cd047887f31e8e94d42375bc243c95964fc4d
Anar Seferov <1anarseferov@gmail.com>
2020-12-05 21:58:34 +0400

    Updates

---
+++ precache-manifest.3cfb93b3e8f9d0b3c8d7aacdcd06cd05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9e709d9fcf781437a43da905ad57a9",
+    "url": "/image-compressor/index.html"
+  },
+  {
+    "revision": "3a80c39afacd2fb7982f",
+    "url": "/image-compressor/static/css/main.a788d844.chunk.css"
+  },
+  {
+    "revision": "e4d5acc2a9fa5445ffe7",
+    "url": "/image-compressor/static/js/2.6fabd243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-compressor/static/js/2.6fabd243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a80c39afacd2fb7982f",
+    "url": "/image-compressor/static/js/main.8e62e986.chunk.js"
+  },
+  {
+    "revision": "3bf17b4688661c4fae5b",
+    "url": "/image-compressor/static/js/runtime-main.12f4767a.js"
+  }
+]);
